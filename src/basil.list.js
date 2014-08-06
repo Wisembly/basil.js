85a174f43a9d7c9d7677b666b926b19ceb3c1fe4
Guillaume Potier <cobalt2760@gmail.com>
2014-08-06 16:49:00 +0200

    - finished list tests + used tabs instead of spaces

--- src/basil.list.js
+++ src/basil.list.js
@@ -1,144 +1,162 @@
 (function (factory) {
-  if (typeof define === 'function' && define.amd) {
-    define(['basil.js'], factory); // AMD
-  } else if (typeof exports === 'object') {
-    module.exports = factory(require( './basil.js')); // Node
-  } else {
-    factory(window.Basil); // Browser global
-  }
+	if (typeof define === 'function' && define.amd) {
+		define(['basil.js'], factory); // AMD
+	} else if (typeof exports === 'object') {
+		module.exports = factory(require( './basil.js')); // Node
+	} else {
+		factory(window.Basil); // Browser global
+	}
 }(function (Basil) {
-  var BasilList = function (options) {
-    return new Basil.utils.extend(Basil.Storage().init(options), Basil.List(options));
-  };
+	var BasilList = function (options) {
+	return new Basil.utils.extend(Basil.Storage().init(options), Basil.List(options));
+	};
 
-  Basil.List = function () {
-    var _toIndex = function (index, length) {
-        if (index < 0)
-          index = length + index;
+	Basil.List = function () {
+		var _toIndex = function (index, length) {
+			if (index < 0)
+				index = length + index;
 
-        if (index < 0 || index >= length)
-          throw new Error("ERR index out of range");
-        return index;
-    };
+			if (index < 0 || index >= length)
+				throw new Error("ERR index out of range");
 
-    return {
-      _listPlugin: true,
-      blpop: function () {
-        throw new Error('not implemented yet');
-      },
-      brpop: function () {
-        throw new Error('not implemented yet');
-      },
-      brpoplpush: function (source, destination) {
-        throw new Error('not implemented yet');
-      },
-      lindex: function (key, index) {
-        var list = this.get(key) || [];
-        index = _toIndex(index, list.length);
+			return index;
+		};
 
-        return list[index] || null;
-      },
-      linsert: function (key, where, pivot, value) {
-        var index = -1,
-          list = this.get(key) || [];
+		return {
+			_listPlugin: true,
+			blpop: function () {
+				throw new Error('not implemented yet');
+			},
+			brpop: function () {
+				throw new Error('not implemented yet');
+			},
+			brpoplpush: function (source, destination) {
+				throw new Error('not implemented yet');
+			},
+			lindex: function (key, index) {
+				var list = this.get(key) || [];
 
-        if ('BEFORE' !== where && 'AFTER' !== where)
-          throw new Error('AFTER|BEFORE');
+				try {
+					index = _toIndex(index, list.length);
+				} catch (e) {
+					return null;
+				}
 
-        if ('string' !== typeof value && 'number' !== typeof value)
-          throw new Error('supported values are only strings and numbers');
+				return list[index];
+			},
+			linsert: function (key, where, pivot, value) {
+				var index = -1,
+					list = this.get(key) || [];
 
-        for (var i = 0; i < list.length; i++) {
-          if (pivot === list[i]) {
-            index = i;
-            break;
-          }
-        }
+				if ('BEFORE' !== where && 'AFTER' !== where)
+					throw new Error('AFTER|BEFORE');
 
-        if (-1 === index)
-          return index;
+				if ('string' !== typeof value && 'number' !== typeof value)
+					throw new Error('supported values are only strings and numbers');
 
-        list.splice('AFTER' === where ? i - 1 : i, 0, value);
-        this.set(key, list);
+				for (var i = 0; i < list.length; i++) {
+					if (pivot === list[i]) {
+						index = i;
+						break;
+					}
+				}
 
-        return list.length;
-      },
-      llen: function (key) {
-        return (this.get(key) || []).length;
-      },
-      lpop: function (key) {
-        var list = this.get(key) || [],
-          value = list.shift();
-        this.set(key, list);
+				if (-1 === index)
+					return index;
 
-        return value || null;
-      },
-      lpush: function (key, value) {
-        var list = this.get(key) || [];
-        list.unshift(value);
-        this.set(key, list);
+				list.splice('AFTER' === where ? i - 1 : i, 0, value);
+				this.set(key, list);
 
-        return list.length;
-      },
-      lpushx: function (key, value) {
-        throw new Error('not implemented yet');
-      },
-      lrange: function (key, start, stop) {
-        var list = this.get(key) || [];
+				return list.length;
+			},
+			llen: function (key) {
+				return (this.get(key) || []).length;
+			},
+			lpop: function (key) {
+				var list = this.get(key) || [],
+					value = list.shift();
 
-        if (-1 === stop)
-          return list.slice(start);
+				if (0 === list.length)
+					this.remove(key);
+				else
+					this.set(key, list);
 
-        return list.slice(start, stop + 1);
-      },
-      lrem: function (key, count, value) {
-        throw new Error('not implemented yet');
-      },
-      lset: function (key, index, value) {
-        var list = this.get(key) || [];
-        index = _toIndex(index, list.length);
-        list[index] = value;
-        this.set(key, list);
+				return value || null;
+			},
+			lpush: function (key, value) {
+				var list = this.get(key) || [];
+				list.unshift(value);
+				this.set(key, list);
 
-        return 'OK';
-      },
-      ltrim: function (key, start, stop) {
-        throw new Error('not implemented yet');
-      },
-      rpop: function (key) {
-        var list = this.get(key) || [],
-          value = list.pop();
-        this.set(key, list);
+				return list.length;
+			},
+			lpushx: function (key, value) {
+				throw new Error('not implemented yet');
+			},
+			lrange: function (key, start, stop) {
+				var list = this.get(key) || [];
 
-        return value || null;
-      },
-      rpoplpush: function (source, destination) {
-        throw new Error('not implemented yet');
-      },
-      rpush: function (key, value) {
-        var list = this.get(key) || [];
-        list.push(value);
-        this.set(key, list);
+				if (-1 === stop)
+					return list.slice(start);
 
-        return list.length;
-      },
-      rpushx: function (key, value) {
-        throw new Error('not implemented yet');
-      }
-    };
-  };
+				return list.slice(start, stop + 1);
+			},
+			lrem: function (key, count, value) {
+				throw new Error('not implemented yet');
+			},
+			lset: function (key, index, value) {
+				var list = this.get(key) || [];
 
-  // browser export
-  window.Basil = BasilList;
+				if (0 === list.length)
+					throw new Error('ERR no such key');
 
-  // AMD export
-  if (typeof define === 'function' && define.amd) {
-    define(function() {
-      return BasilList;
-    });
-  // commonjs export
-  } else if (typeof module !== 'undefined' && module.exports) {
-    module.exports = BasilList;
-  }
+				index = _toIndex(index, list.length);
+				list[index] = value;
+				this.set(key, list);
+
+				return 'OK';
+			},
+			ltrim: function (key, start, stop) {
+				throw new Error('not implemented yet');
+			},
+			rpop: function (key) {
+				var list = this.get(key) || [],
+					value = list.pop();
+
+				if (0 === list.length)
+					this.remove(key);
+				else
+					this.set(key, list);
+
+				return value || null;
+			},
+			rpoplpush: function (source, destination) {
+				throw new Error('not implemented yet');
+			},
+			rpush: function (key, value) {
+				var list = this.get(key) || [];
+				list.push(value);
+				this.set(key, list);
+
+				return list.length;
+			},
+			rpushx: function (key, value) {
+				throw new Error('not implemented yet');
+			}
+		};
+	};
+
+	// browser export
+	window.Basil = BasilList;
+
+	// AMD export
+	if (typeof define === 'function' && define.amd) {
+		define(function() {
+			return BasilList;
+		});
+	// commonjs export
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = BasilList;
+	}
 
 }));
--- test/list.js
+++ test/list.js
@@ -1,57 +1,107 @@
 (function () {
 
-  describe('Basil List test suite', function () {
-    it('should have Basil List plugin loaded', function () {
-      var basil = new window.Basil();
-      expect(basil._listPlugin).to.be(true);
-    });
-    it.skip('should have blpop method');
-    it.skip('should have brpop method');
-    it.skip('should have brpoplpush method');
-    it('should have lindex method', function () {
-      var basil = new window.Basil();
-      basil.set('foo', ['foo', 'bar']);
-      expect(basil.lindex('foo', -42)).to.be(null);
-      expect(basil.lindex('foo', -1)).to.be('bar');
-      expect(basil.lindex('foo', 0)).to.be('foo');
-      expect(basil.lindex('foo', 1)).to.be('bar');
-      expect(basil.lindex('foo', 42)).to.be(null);
-    });
-    it('should have linsert method', function () {
-      var basil = new window.Basil();
-      basil.set('foo', ['foo', 'bar']);
-      expect(basil.linsert('foo', 'AFTER', 'foo', 'baz')).to.be(3);
-      expect(basil.get('foo')).to.be.eql(['foo', 'baz', 'bar']);
+	describe('Basil List test suite', function () {
+		var basil = new window.Basil();
 
-      try {
-        basil.linsert('foo', 'bar');
-        expect(true).to.be(false);
-      } catch (e) {
-        expect(e.message).to.be('AFTER|BEFORE');
-      }
+		beforeEach(function () {
+			basil.set('key', ['foo', 'bar']);
+		});
+		it('should have Basil List plugin loaded', function () {
+			expect(basil._listPlugin).to.be(true);
+		});
+		it.skip('should have blpop method');
+		it.skip('should have brpop method');
+		it.skip('should have brpoplpush method');
+		it('should have lindex method', function () {
+			expect(basil.lindex('key', -42)).to.be(null);
+			expect(basil.lindex('key', -1)).to.be('bar');
+			expect(basil.lindex('key', 0)).to.be('foo');
+			expect(basil.lindex('key', 1)).to.be('bar');
+			expect(basil.lindex('key', 42)).to.be(null);
+		});
+		it('should have linsert method', function () {
+			expect(basil.linsert('key', 'AFTER', 'foo', 'baz')).to.be(3);
+			expect(basil.get('key')).to.be.eql(['foo', 'baz', 'bar']);
 
-      try {
-        basil.linsert('foo', 'AFTER');
-        expect(true).to.be(false);
-      } catch (e) {
-        expect(e.message).to.be('supported values are only strings and numbers');
-      }
+			try {
+				basil.linsert('key', 'bar');
+				expect().fail("should not pass");
+			} catch (e) {
+				expect(e.message).to.be('AFTER|BEFORE');
+			}
 
-      expect(basil.linsert('foo', 'BEFORE', 'foo', 'bux')).to.be(4);
-      expect(basil.get('foo')).to.be.eql(['bux', 'foo', 'baz', 'bar']);
-    });
-    it.skip('should have llen method');
-    it.skip('should have lpop method');
-    it.skip('should have lpush method');
-    it.skip('should have lpushx method');
-    it.skip('should have lrange method');
-    it.skip('should have lrem method');
-    it.skip('should have lset method');
-    it.skip('should have ltrim method');
-    it.skip('should have rpop method');
-    it.skip('should have brpoplpush method');
-    it.skip('should have rpush method');
-    it.skip('should have rpushx method');
-  });
+			try {
+				basil.linsert('key', 'AFTER');
+				expect().fail("should not pass");
+			} catch (e) {
+				expect(e.message).to.be('supported values are only strings and numbers');
+			}
+
+			expect(basil.linsert('key', 'BEFORE', 'foo', 'bux')).to.be(4);
+			expect(basil.get('key')).to.be.eql(['bux', 'foo', 'baz', 'bar']);
+		});
+		it('should have llen method', function () {
+			expect(basil.llen('key')).to.be(2);
+			basil.set('key', []);
+			expect(basil.llen('key')).to.be(0);
+		});
+		it('should have lpop method', function () {
+			expect(basil.lpop('key')).to.be('foo');
+			expect(basil.llen('key')).to.be(1);
+			expect(basil.lpop('key')).to.be('bar');
+			expect(basil.get('key')).to.be(null); // verify that key is well deleted if empty
+		});
+		it('should have lpush method', function () {
+			expect(basil.lpush('key', 'baz')).to.be(3);
+			expect(basil.get('key')).to.eql(['baz', 'foo', 'bar']);
+		});
+		it.skip('should have lpushx method');
+		it('should have lrange method', function () {
+			basil.rpush('key', 'baz');
+			expect(basil.lrange('key', 0, 0)).to.eql(['foo']);
+			expect(basil.lrange('key', 1, 1)).to.eql(['bar']);
+			expect(basil.lrange('key', 2, 2)).to.eql(['baz']);
+			expect(basil.lrange('key', 0, 2)).to.eql(['foo', 'bar', 'baz']);
+			expect(basil.lrange('key', 0, 100)).to.eql(['foo', 'bar', 'baz']);
+			expect(basil.lrange('key', -100, 100)).to.eql(['foo', 'bar', 'baz']);
+			expect(basil.lrange('key', 5, 10)).to.eql([]);
+		});
+		it.skip('should have lrem method');
+		it('should have lset method', function () {
+			try {
+				basil.lset('donotexist', 0, 'foo');
+				expect.tail("should not pass");
+			} catch (e) {
+				expect(e.message).to.be('ERR no such key');
+			}
+
+			try {
+				basil.lset('key', 2, 'baz');
+				expect().fail("should not pass");
+			} catch (e) {
+				expect(e.message).to.be('ERR index out of range');
+			}
+			expect(basil.lset('key', 1, 'baz')).to.be('OK');
+			expect(basil.lset('key', -2, 'bux')).to.be('OK');
+			expect(basil.lrange('key', 0, 2)).to.eql(['bux', 'baz']);
+		});
+		it.skip('should have ltrim method');
+		it('should have rpop method', function () {
+			expect(basil.rpop('key')).to.be('bar');
+			expect(basil.llen('key')).to.be(1);
+			expect(basil.rpop('key')).to.be('foo');
+			expect(basil.llen('key')).to.be(0);
+			expect(basil.get('key')).to.be(null); // verify that key is well deleted if empty
+		});
+		it.skip('should have brpoplpush method');
+		it('should have rpush method', function () {
+			expect(basil.rpush('key', 'baz')).to.be(3);
+			expect(basil.lindex('key', 2)).to.be('baz');
+		});
+		it.skip('should have rpushx method');
+		afterEach(function () {
+			basil.set('key', []);
+		});
+	});
 
 }());
